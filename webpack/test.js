a7093ca17c71ab3a6b3940b81857479d45a7bcf2
Fabio Spampinato <spampinabio@gmail.com>
2016-10-24 22:48:32 +0200

    Fixed PhantomJS
    By using `const` instead of `let`

--- webpack/test.js
+++ webpack/test.js
@@ -6,5 +6,5 @@ require ( 'isomorphic-fetch' );
 
 /* TESTS */
 
-let context = require.context ( '../src', true, /(.*\.)?(app-)?tests?\.tsx?$/ );
+const context = require.context ( '../src', true, /(.*\.)?(app-)?tests?\.tsx?$/ );
 context.keys ().forEach ( context );
