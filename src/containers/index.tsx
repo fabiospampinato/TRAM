34a78653952059cdc29f6b5a4ea51f2e6ad0a285
Fabio Spampinato <spampinabio@gmail.com>
2016-10-24 22:46:02 +0200

    Fixed some imports modules cases

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,7 @@
 /** Exporting Containers for Easier Imports */
-export { Html } from './Html';
-export { App } from './App';
-export { Home } from './Home';
-export { About } from './About';
-export { Counter } from './Counter';
-export { Stars } from './Stars';
+export { Html } from './html';
+export { App } from './app';
+export { Home } from './home';
+export { About } from './about';
+export { Counter } from './counter';
+export { Stars } from './stars';
