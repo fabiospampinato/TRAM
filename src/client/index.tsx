297a6d2414a086245298417abe2b08f0f1a26d48
Fabio Spampinato <spampinabio@gmail.com>
2016-11-03 19:20:03 +0100

    Exporting rendered into client/rendered
    In order to better provide HMR

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,31 +1,25 @@
 
 /* IMPORT */
 
-import * as React from 'react';
-import {ApolloProvider} from 'react-apollo';
-import * as ReactDOM from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import {browserHistory} from 'react-router';
 import {syncHistoryWithStore} from 'react-router-redux';
-import {ReduxAsyncConnect} from 'redux-connect';
 import Client from '../api/client';
 import {configureStore} from '../redux/store';
-import routes from '../routes';
+import renderer from './renderer';
 
 /* VARIABLES */
 
 let store: Redux.Store<any> = configureStore ( browserHistory, window.__INITIAL_STATE__ ),
-    history = syncHistoryWithStore ( browserHistory, store ),
-    root = document.getElementById ( 'app' );
-
-if ( !root ) throw new Error ( 'Missing app root' );
+    history = syncHistoryWithStore ( browserHistory, store );
 
 /* RENDER */
 
-ReactDOM.render (
-  <ApolloProvider store={store} client={Client} key="provider">
-    <Router history={history} render={props => <ReduxAsyncConnect {...props} />}>
-      {routes}
-    </Router>
-  </ApolloProvider>,
-  root
-);
+renderer ( store, Client, history );
+
+/* HOT */
+
+if ( module.hot ) {
+  module.hot.accept ( './renderer', function () {
+    require ( './renderer').default ( store, Client, history );
+  });
+}
---
+++ src/client/renderer.tsx
@@ -0,0 +1,34 @@
+
+/* IMPORT */
+
+import * as React from 'react';
+import {ApolloProvider} from 'react-apollo';
+import {render} from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+import {Router} from 'react-router';
+import routes from '../routes';
+
+/* RENDERER */
+
+let renderer = function ( store, client, history ) {
+
+  let root = document.getElementById ( 'app' );
+
+  if ( !root ) throw new Error ( 'Missing app root' );
+
+  render (
+    <AppContainer>
+      <ApolloProvider store={store} client={client} key="provider">
+        <Router history={history}>
+          {routes}
+        </Router>
+      </ApolloProvider>
+    </AppContainer>,
+    root
+  );
+
+};
+
+/* EXPORT */
+
+export default renderer;
