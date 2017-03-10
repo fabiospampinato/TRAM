047060b513ccfc83f4f794ad716f37fc95915071
Fabio Spampinato <spampinabio@gmail.com>
2017-03-10 19:02:43 +0100

    Add a `template` directory

---
+++ src/ui/template/index.ts
@@ -0,0 +1,14 @@
+
+/* ================================================================================
+ * TRAM - UI - Template
+ * ================================================================================
+ * Copyright (c) 2016-present Fabio Spampinato
+ * Licensed under MIT (https://github.com/fabiospampinato/TRAM/blob/master/LICENSE)
+ * ================================================================================ */
+
+/* IMPORT */
+
+import './template.js';
+import './template.ts';
+import './template.css';
+import './template.scss';
