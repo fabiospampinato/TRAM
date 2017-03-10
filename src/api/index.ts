e38ac150d5260dd51d17437e9965fc564af91d01
Fabio Spampinato <spampinabio@gmail.com>
2017-03-10 22:14:12 +0100

    Updated copyright years

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@
 /* ================================================================================
  * TRAM - API
  * ================================================================================
- * Copyright (c) 2016-2017 Fabio Spampinato
+ * Copyright (c) 2016-present Fabio Spampinato
  * Licensed under MIT (https://github.com/fabiospampinato/TRAM/blob/master/LICENSE)
  * ================================================================================ */
 
